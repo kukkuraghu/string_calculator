75e2ff76b56040591867d23596c5b9772bed1e65
Raghunath Nedumpurath <raghu.ned@gmail.com>
2022-06-04 20:24:39 +0530

    Refactor StringCalculator

--- src/main/java/com/nedumpurath/StringCalculator.java
+++ src/main/java/com/nedumpurath/StringCalculator.java
@@ -7,22 +7,27 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
     public static int add(String textInput) {
         if (textInput.isBlank()) return 0;
+
         var numberStrings = textInput.split(",");
-        var firstNumberString = numberStrings[0].strip();
-        var firstNumberBigInteger = new BigInteger(firstNumberString); //if firstNumberString is not an integer, this will throw NumberFormatException
-        var secondNumberString = "";
+        var firstNumberStringStripped = numberStrings[0].strip();
+        var firstNumberBigInteger = new BigInteger(firstNumberStringStripped); //if firstNumberString is not an integer, this will throw NumberFormatException
+
+        //throw ArithmeticException, if the number is not in the integer range supported
         if (moreThanMaxInt(firstNumberBigInteger) || lessThanMinInt(firstNumberBigInteger)) {
             throw new ArithmeticException();
         }
         if (numberStrings.length == 1) {
-            return Math.addExact(parseInt(firstNumberString), 0); //firstNumberString has a valid integer. But if it is not in the Java integer range, parseInt will throw NumberFormatException
+            return Math.addExact(parseInt(firstNumberStringStripped), 0); //firstNumberString has a valid integer. But if it is not in the Java integer range, parseInt will throw NumberFormatException
         }
-        secondNumberString = numberStrings[1].strip();
-        var secondNumberBigInteger = new BigInteger(secondNumberString); //if secondNumberString is not an integer, this will throw NumberFormatException
+
+        var secondNumberStringStripped = numberStrings[1].strip();
+        var secondNumberBigInteger = new BigInteger(secondNumberStringStripped); //if secondNumberString is not an integer, this will throw NumberFormatException
+        //throw ArithmeticException, if the number is not in the integer range supported
         if (moreThanMaxInt(secondNumberBigInteger) || lessThanMinInt(secondNumberBigInteger)) {
             throw new ArithmeticException();
         }
-        return Math.addExact(parseInt(firstNumberString), parseInt(secondNumberString));
+
+        return Math.addExact(parseInt(firstNumberStringStripped), parseInt(secondNumberStringStripped)); //addExact will throw ArithmeticException, if the sum is more than MAXVALUE or less than MINVALUE
     }
 
     private  static Boolean moreThanMaxInt(BigInteger number) {
