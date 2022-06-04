c217420b6dbd26850a4c70cb379d5846e00be8fb
Raghunath Nedumpurath <raghu.ned@gmail.com>
2022-06-04 15:26:34 +0530

    Make the Test Pass

--- src/main/java/com/nedumpurath/StringCalculator.java
+++ src/main/java/com/nedumpurath/StringCalculator.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
     public static int add(String textInput) {
+        if (textInput.isBlank()) return 0;
         var numberStrings = textInput.split(",");
         if (numberStrings.length == 1) return parseInt(numberStrings[0]);
         return parseInt(numberStrings[0])+ parseInt(numberStrings[1]);
