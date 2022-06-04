b8731b7eeab4cb07ab79c27a33937ae32b57fac0
Raghunath Nedumpurath <raghu.ned@gmail.com>
2022-06-04 20:05:34 +0530

    Make the test addUpToTwoNumberInvokedWithIntegersWhoseSumLessThanMinInt pass

--- src/main/java/com/nedumpurath/StringCalculator.java
+++ src/main/java/com/nedumpurath/StringCalculator.java
@@ -11,16 +11,21 @@ public class StringCalculator {
         var firstNumberString = numberStrings[0].strip();
         var firstNumberBigInteger = new BigInteger(firstNumberString); //if firstNumberString is not an integer, this will throw NumberFormatException
         var secondNumberString = "";
+        if (firstNumberBigInteger.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0 ) {
+            throw new ArithmeticException();
+        }
+        if (firstNumberBigInteger.compareTo(new BigInteger(String.valueOf(Integer.MIN_VALUE))) < 0 ) {
+            throw new ArithmeticException();
+        }
         if (numberStrings.length == 1) {
-
-            if (firstNumberBigInteger.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0 ) {
-                throw new ArithmeticException();
-            }
             return Math.addExact(parseInt(firstNumberString), 0); //firstNumberString has a valid integer. But if it is not in the Java integer range, parseInt will throw NumberFormatException
         }
         secondNumberString = numberStrings[1].strip();
         var secondNumberBigInteger = new BigInteger(secondNumberString); //if secondNumberString is not an integer, this will throw NumberFormatException
         if (secondNumberBigInteger.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0 ) {
+            throw new ArithmeticException();
+        }
+        if (secondNumberBigInteger.compareTo(new BigInteger(String.valueOf(Integer.MIN_VALUE))) < 0 ) {
             throw new ArithmeticException();
         }
         return Math.addExact(parseInt(firstNumberString), parseInt(secondNumberString));
