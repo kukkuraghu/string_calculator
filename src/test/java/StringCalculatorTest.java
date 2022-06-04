a30d73c6bc59a43a08a1fc83e2e5edf13932a554
Raghunath Nedumpurath <raghu.ned@gmail.com>
2022-06-04 15:02:09 +0530

    Create Base Version

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    void addUpToTwoNumber() {
+        assertEquals(5, StringCalculator.add("2,3"), "when the input string has two integers separated comma, StringCalculator.add should return the sum of the two numbers");
+    }
+}
